0802e8f7f02b40b115483fd2e344529df46c8f35
Eric Brandwein <brandweineric@gmail.com>
2018-02-23 01:58:28 -0300

    Agrega ObjetivoDeOcupar5

---
+++ src/main/java/objetivos/ObjetivoDeOcupar5.kt
@@ -0,0 +1,38 @@
+package objetivos
+
+import Jugador
+import paises.Continente
+import paises.PaisEnJuego
+
+class ObjetivoDeOcupar5 : ObjetivoDeOcupar {
+    override val descripcion =
+            "Ocupar 4 países de América del Norte, 4 de Europa, 4 de Asia, " +
+                    "3 de América del Sur, 3 de América Central, " +
+                    "3 de Africa y 3 de Oceanía."
+
+    override fun cumplido(paises: List<PaisEnJuego>, conquistado: PaisEnJuego,
+            jugadores: List<Jugador>, jugadorActual: Int): Boolean {
+        val cantPaisesAmericaDelNorte = cantPaisesDeContinenteConDueno(
+                paises, Continente.AMERICA_DEL_NORTE, jugadorActual,
+                conquistado)
+        val cantPaisesEuropa = cantPaisesDeContinenteConDueno(
+                paises, Continente.EUROPA, jugadorActual, conquistado)
+        val cantPaisesAsia = cantPaisesDeContinenteConDueno(
+                paises, Continente.ASIA, jugadorActual, conquistado)
+        val cantPaisesAmericaDelSur = cantPaisesDeContinenteConDueno(
+                paises, Continente.AMERICA_DEL_SUR, jugadorActual, conquistado)
+        val cantPaisesAmericaCentral = cantPaisesDeContinenteConDueno(
+                paises, Continente.AMERICA_CENTRAL, jugadorActual, conquistado)
+        val cantPaisesAfrica = cantPaisesDeContinenteConDueno(
+                paises, Continente.AFRICA, jugadorActual, conquistado)
+        val cantPaisesOceania = cantPaisesDeContinenteConDueno(
+                paises, Continente.OCEANIA, jugadorActual, conquistado)
+        return cantPaisesAmericaDelNorte >= 4 &&
+                cantPaisesEuropa >= 4 &&
+                cantPaisesAsia >= 4 &&
+                cantPaisesAmericaDelSur >= 3 &&
+                cantPaisesAmericaCentral >= 3 &&
+                cantPaisesAfrica >= 3 &&
+                cantPaisesOceania >= 3
+    }
+}
