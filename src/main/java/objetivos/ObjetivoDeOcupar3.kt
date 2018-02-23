fddaa5e6f8d0ecb19c8ecb3c7cdb3dee76dd6c88
Eric Brandwein <brandweineric@gmail.com>
2018-02-23 01:29:43 -0300

    Agrega ObjetivoDeOcupar3

---
+++ src/main/java/objetivos/ObjetivoDeOcupar3.kt
@@ -0,0 +1,18 @@
+package objetivos
+
+import Jugador
+import paises.Continente
+import paises.PaisEnJuego
+
+class ObjetivoDeOcupar3 : ObjetivoDeOcupar {
+    override val descripcion = "Ocupar Asia y América Central."
+
+    override fun cumplido(paises: List<PaisEnJuego>, conquistado: PaisEnJuego,
+            jugadores: List<Jugador>, jugadorActual: Int): Boolean {
+        val asiaConquistado = continenteConquistado(
+                paises, Continente.ASIA, jugadorActual, conquistado)
+        val americaCentralConquistado = continenteConquistado(
+                paises, Continente.AMERICA_CENTRAL, jugadorActual, conquistado)
+        return asiaConquistado && americaCentralConquistado
+    }
+}
