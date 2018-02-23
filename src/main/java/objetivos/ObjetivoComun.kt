7ed507d91bd8a9b3ddedbe1e4f9a600169da9997
Eric Brandwein <brandweineric@gmail.com>
2018-02-23 02:07:46 -0300

    Agrega ObjetivoDeOcupar6

--- src/main/java/objetivos/ObjetivoComun.kt
+++ src/main/java/objetivos/ObjetivoComun.kt
@@ -3,15 +3,11 @@ package objetivos
 import Jugador
 import paises.PaisEnJuego
 
-class ObjetivoComun : Objetivo {
+class ObjetivoComun : ObjetivoDeOcupar {
     override val descripcion = "Ocupar 45 países."
 
     override fun cumplido(paises: List<PaisEnJuego>, conquistado: PaisEnJuego,
                           jugadores: List<Jugador>,
                           jugadorActual: Int): Boolean =
             cantPaisesConDueno(paises, jugadorActual) + 1 >= 45
-
-    override fun puedeCumplirse(paises: List<PaisEnJuego>,
-                                jugadores: List<Jugador>,
-                                jugadorActual: Int): Boolean = true
 }
---
+++ src/main/java/objetivos/ObjetivoDeOcupar6.kt
@@ -0,0 +1,23 @@
+package objetivos
+
+import Jugador
+import paises.Continente
+import paises.PaisEnJuego
+
+class ObjetivoDeOcupar6 : ObjetivoDeOcupar {
+    override val descripcion = "Ocupar Oceanía, 6 países de Asia, " +
+            "6 de Africa y 6 de América del Norte."
+
+    override fun cumplido(paises: List<PaisEnJuego>, conquistado: PaisEnJuego,
+            jugadores: List<Jugador>, jugadorActual: Int): Boolean {
+        val oceaniaConquistado = continenteConquistado(
+                paises, Continente.OCEANIA, jugadorActual, conquistado)
+        val cantDeAfricaConquistados = cantPaisesDeContinenteConDueno(
+                paises, Continente.AFRICA, jugadorActual, conquistado)
+        val cantDeAmericaDelNorteConquistados = cantPaisesDeContinenteConDueno(
+                paises, Continente.AMERICA_DEL_NORTE, jugadorActual,
+                conquistado)
+        return oceaniaConquistado && cantDeAfricaConquistados >= 6
+                && cantDeAmericaDelNorteConquistados >= 6
+    }
+}
