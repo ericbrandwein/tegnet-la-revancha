445c310f50585a1610c8710571d0520a4801c954
Eric Brandwein <brandweineric@gmail.com>
2018-03-19 23:31:24 -0300

    Adapta ObjetivoDeOcupar1 a mapa de continentes con cantidad de paises

--- src/main/java/objetivos/ObjetivoDeOcupar1.kt
+++ src/main/java/objetivos/ObjetivoDeOcupar1.kt
@@ -1,20 +1,14 @@
 package objetivos
 
-import juego.Jugador
 import paises.Continente
-import paises.PaisEnJuego
-import paises.continenteOcupado
 
-class ObjetivoDeOcupar1 : ObjetivoDeOcupar {
+class ObjetivoDeOcupar1 : ObjetivoDeOcupar() {
     override val descripcion = "Ocupar Europa y América del Sur"
 
-    override fun cumplido(paises: Iterable<PaisEnJuego>,
-            jugadores: List<Jugador>, jugadorActual: Int,
-            jugadorAtacado: Int): Boolean {
-        val europaConquistada = continenteOcupado(
-                paises, Continente.EUROPA, jugadorActual)
-        val americaDelSurConquistada = continenteOcupado(
-                paises, Continente.AMERICA_DEL_SUR, jugadorActual)
-        return europaConquistada && americaDelSurConquistada
+    init {
+        paisesDeContinentesAOcupar = mapOf(
+                Continente.EUROPA to TODOS_LOS_PAISES,
+                Continente.AMERICA_DEL_SUR to TODOS_LOS_PAISES
+        )
     }
 }
