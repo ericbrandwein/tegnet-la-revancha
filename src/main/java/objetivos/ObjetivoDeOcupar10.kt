ba09d03946e7daa0e8d81211c98817564c387dcd
Eric Brandwein <brandweineric@gmail.com>
2018-03-19 23:43:32 -0300

    Adapta ObjetivoDeOcupar10

--- src/main/java/objetivos/ObjetivoDeOcupar10.kt
+++ src/main/java/objetivos/ObjetivoDeOcupar10.kt
@@ -1,26 +1,16 @@
 package objetivos
 
-import juego.Jugador
 import paises.Continente
-import paises.PaisEnJuego
-import paises.cantPaisesDeContinenteConDuenoMasConquistado
-import paises.continenteConquistado
 
-class ObjetivoDeOcupar10 : ObjetivoDeOcupar {
+class ObjetivoDeOcupar10 : ObjetivoDeOcupar() {
     override val descripcion =
             "Ocupar Europa, 4 países de Asia, 4 países de América del Sur."
 
-    override fun cumplido(paises: List<PaisEnJuego>, conquistado: PaisEnJuego,
-            jugadores: List<Jugador>, jugadorActual: Int): Boolean {
-        val europaConquistado = continenteConquistado(
-                paises, Continente.EUROPA, jugadorActual, conquistado)
-        val cantAsiaConquistado = cantPaisesDeContinenteConDuenoMasConquistado(
-                paises, Continente.ASIA, jugadorActual, conquistado)
-        val cantAmericaDelSurConquistado =
-                cantPaisesDeContinenteConDuenoMasConquistado(
-                        paises, Continente.AMERICA_DEL_SUR, jugadorActual,
-                        conquistado)
-        return europaConquistado && cantAsiaConquistado >= 4 &&
-                cantAmericaDelSurConquistado >= 4
+    init {
+        paisesDeContinentesAOcupar = mapOf(
+                Continente.EUROPA to TODOS_LOS_PAISES,
+                Continente.ASIA to 4,
+                Continente.AMERICA_DEL_SUR to 4
+        )
     }
 }
