3c952de648630446688c0f125a50ebb8be8c7fe0
Eric Brandwein <brandweineric@gmail.com>
2018-03-19 23:42:29 -0300

    Adapta ObjetivoDeOcupar9

--- src/main/java/objetivos/ObjetivoDeOcupar9.kt
+++ src/main/java/objetivos/ObjetivoDeOcupar9.kt
@@ -1,28 +1,17 @@
 package objetivos
 
-import juego.Jugador
 import paises.Continente
-import paises.PaisEnJuego
-import paises.cantPaisesDeContinenteConDuenoMasConquistado
-import paises.continenteConquistado
 
-class ObjetivoDeOcupar9 : ObjetivoDeOcupar {
+class ObjetivoDeOcupar9 : ObjetivoDeOcupar() {
     override val descripcion =
             "Ocupar Oceanía, Africa, 4 países de América Central y 4 de Asia."
 
-    override fun cumplido(paises: List<PaisEnJuego>, conquistado: PaisEnJuego,
-            jugadores: List<Jugador>, jugadorActual: Int): Boolean {
-        val oceaniaConquistado = continenteConquistado(
-                paises, Continente.OCEANIA, jugadorActual, conquistado)
-        val africaConquistado = continenteConquistado(
-                paises, Continente.AFRICA, jugadorActual, conquistado)
-        val cantAmericaCentralConquistado =
-                cantPaisesDeContinenteConDuenoMasConquistado(
-                        paises, Continente.AMERICA_CENTRAL, jugadorActual,
-                        conquistado)
-        val cantAsiaConquistado = cantPaisesDeContinenteConDuenoMasConquistado(
-                paises, Continente.ASIA, jugadorActual, conquistado)
-        return oceaniaConquistado && africaConquistado &&
-                cantAmericaCentralConquistado >= 4 && cantAsiaConquistado >= 4
+    init {
+        paisesDeContinentesAOcupar = mapOf(
+                Continente.OCEANIA to TODOS_LOS_PAISES,
+                Continente.AFRICA to TODOS_LOS_PAISES,
+                Continente.AMERICA_CENTRAL to 4,
+                Continente.ASIA to 4
+        )
     }
 }
