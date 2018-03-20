de3532d54ffcf257827390108a63a6cda73bfccb
Eric Brandwein <brandweineric@gmail.com>
2018-03-19 22:40:09 -0300

    Arregla firma de metodos en ObjetivoDeOcupar1

--- src/main/java/objetivos/ObjetivoDeOcupar1.kt
+++ src/main/java/objetivos/ObjetivoDeOcupar1.kt
@@ -3,17 +3,18 @@ package objetivos
 import juego.Jugador
 import paises.Continente
 import paises.PaisEnJuego
-import paises.continenteConquistado
+import paises.continenteOcupado
 
 class ObjetivoDeOcupar1 : ObjetivoDeOcupar {
     override val descripcion = "Ocupar Europa y América del Sur"
 
-    override fun cumplido(paises: List<PaisEnJuego>, conquistado: PaisEnJuego,
-            jugadores: List<Jugador>, jugadorActual: Int): Boolean {
-        val europaConquistada = continenteConquistado(
-                paises, Continente.EUROPA, jugadorActual, conquistado)
-        val americaDelSurConquistada = continenteConquistado(
-                paises, Continente.AMERICA_DEL_SUR, jugadorActual, conquistado)
+    override fun cumplido(paises: Iterable<PaisEnJuego>,
+            jugadores: List<Jugador>, jugadorActual: Int,
+            jugadorAtacado: Int): Boolean {
+        val europaConquistada = continenteOcupado(
+                paises, Continente.EUROPA, jugadorActual)
+        val americaDelSurConquistada = continenteOcupado(
+                paises, Continente.AMERICA_DEL_SUR, jugadorActual)
         return europaConquistada && americaDelSurConquistada
     }
 }
