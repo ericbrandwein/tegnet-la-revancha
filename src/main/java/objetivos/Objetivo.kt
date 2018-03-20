b122dcd2d958568b572200c0c0e975fcd19182c8
Eric Brandwein <brandweineric@gmail.com>
2018-03-19 22:17:35 -0300

    Cambia firma de Objetivo.cumplido
    Ahora se debe llamar despues de conquistar el pais, o sea, despues de que cambie el dueño

--- src/main/java/objetivos/Objetivo.kt
+++ src/main/java/objetivos/Objetivo.kt
@@ -7,18 +7,18 @@ interface Objetivo {
     val descripcion: String
 
     /**
-     * Determina si se habra cumplido el objetivo del jugador actual
-     * despues de haber conquistado el pais.
+     * Determina si se cumplio el objetivo del [jugadorActual]
+     * despues de haber conquistado un pais del [jugadorAtacado].
      *
      * @param jugadores lista de jugadores en orden de la vuelta, tal que
      * `jugadores[i + 1]` esta a la izquierda de `jugadores[i]`
      */
-    fun cumplido(paises: List<PaisEnJuego>, conquistado: PaisEnJuego,
-                 jugadores: List<Jugador>, jugadorActual: Int): Boolean
+    fun cumplido(paises: Iterable<PaisEnJuego>, jugadores: List<Jugador>,
+            jugadorActual: Int, jugadorAtacado: Int): Boolean
 
     /**
      * Determina si se puede cumplir el objetivo.
      */
     fun puedeCumplirse(paises: List<PaisEnJuego>, jugadores: List<Jugador>,
-                       jugadorActual: Int): Boolean
+            jugadorActual: Int): Boolean
 }
