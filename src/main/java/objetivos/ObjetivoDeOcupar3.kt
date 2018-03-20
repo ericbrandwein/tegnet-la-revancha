ba13672691208bd3ab0b3041256e25424f62bfbc
Eric Brandwein <brandweineric@gmail.com>
2018-03-19 23:34:52 -0300

    Adapta ObjetivoDeOcupar3

--- src/main/java/objetivos/ObjetivoDeOcupar3.kt
+++ src/main/java/objetivos/ObjetivoDeOcupar3.kt
@@ -1,19 +1,14 @@
 package objetivos
 
-import juego.Jugador
 import paises.Continente
-import paises.PaisEnJuego
-import paises.continenteConquistado
 
-class ObjetivoDeOcupar3 : ObjetivoDeOcupar {
+class ObjetivoDeOcupar3 : ObjetivoDeOcupar() {
     override val descripcion = "Ocupar Asia y América Central."
 
-    override fun cumplido(paises: List<PaisEnJuego>, conquistado: PaisEnJuego,
-            jugadores: List<Jugador>, jugadorActual: Int): Boolean {
-        val asiaConquistado = continenteConquistado(
-                paises, Continente.ASIA, jugadorActual, conquistado)
-        val americaCentralConquistado = continenteConquistado(
-                paises, Continente.AMERICA_CENTRAL, jugadorActual, conquistado)
-        return asiaConquistado && americaCentralConquistado
+    init {
+        paisesDeContinentesAOcupar = mapOf(
+                Continente.ASIA to TODOS_LOS_PAISES,
+                Continente.AMERICA_CENTRAL to TODOS_LOS_PAISES
+        )
     }
 }
