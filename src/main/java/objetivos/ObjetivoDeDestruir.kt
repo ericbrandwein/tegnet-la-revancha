890c6f5fc6b9de367321fdd499b6389df7e7e37d
Eric Brandwein <brandweineric@gmail.com>
2018-03-19 22:32:21 -0300

    Arregla las firmas de métodos de ObjetivoDeDestruir

--- src/main/java/objetivos/ObjetivoDeDestruir.kt
+++ src/main/java/objetivos/ObjetivoDeDestruir.kt
@@ -5,30 +5,27 @@ import paises.PaisEnJuego
 import paises.cantPaisesConDueno
 
 abstract class ObjetivoDeDestruir : Objetivo {
-    override fun cumplido(paises: List<PaisEnJuego>, conquistado: PaisEnJuego,
-                          jugadores: List<Jugador>,
-                          jugadorActual: Int): Boolean {
+    override fun cumplido(paises: Iterable<PaisEnJuego>,
+            jugadores: List<Jugador>, jugadorActual: Int,
+            jugadorAtacado: Int): Boolean {
         val indiceEnemigo = jugadorADestruir(jugadores, jugadorActual)
-        return esSuUltimoPais(paises, conquistado, indiceEnemigo)
+        return jugadorAtacado == indiceEnemigo &&
+                seQuedoSinPaises(paises, jugadorAtacado)
     }
 
-    override fun puedeCumplirse(paises: List<PaisEnJuego>,
-                                jugadores: List<Jugador>,
-                                jugadorActual: Int): Boolean {
-
+    override fun puedeCumplirse(paises: Iterable<PaisEnJuego>,
+            jugadores: List<Jugador>,
+            jugadorActual: Int): Boolean {
         val indiceEnemigo = jugadorADestruir(jugadores, jugadorActual)
         return !jugadores[indiceEnemigo].perdio
     }
 
-    private fun esSuUltimoPais(paises: List<PaisEnJuego>, pais: PaisEnJuego,
-                               jugador: Int): Boolean {
-        return pais.dueno == jugador &&
-                cantPaisesConDueno(paises, jugador) == 1
-    }
+    private fun seQuedoSinPaises(paises: Iterable<PaisEnJuego>,
+            jugador: Int) = cantPaisesConDueno(paises, jugador) == 0
 
     /**
      * Determina el indice en el arreglo de jugadores al que se debe destruir
      */
     protected abstract fun jugadorADestruir(jugadores: List<Jugador>,
-                                            jugadorActual: Int): Int
+            jugadorActual: Int): Int
 }
